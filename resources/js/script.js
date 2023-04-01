8eeb5112adc8520ea4ab93ec2aa474cc34dcabe5
“Emery <emerygabriel.m@gmail.com>
2023-03-31 23:34:20 -0400

    cosmetic changes
    changed hero image b/c a giant image of me was not the move.

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,12 +1,15 @@
 const nav = document.querySelector('.nav')
 const currentPage = document.querySelectorAll('.page')
+const infoPos = document.getElementById('info')
 window.addEventListener('scroll', fixNav)
 
 function fixNav() {
     if(window.scrollY > nav.offsetHeight + 100) {
         nav.classList.add('active')
+        infoPos.classList.add('active')
     } else {
         nav.classList.remove('active')
+        infoPos.classList.remove('active')
     }
 }
 
